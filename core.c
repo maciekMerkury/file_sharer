fd82d68508a514980d9b297982f90c00964d5449
maciekMerkury <maciekmerkury2@gmail.com>
2024-07-20 15:16:49 +0200

    add more functions and structs to core

--- core.c
+++ core.c
@@ -1,8 +1,11 @@
 // the gnu version of the basename function is needed
 #define _GNU_SOURCE
 
+#include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
+#include <stdint.h>
 
 #include "core.h"
 
@@ -24,3 +27,58 @@ int read_file_data(file_data_t *dst, const char *path)
 
 	return 0;
 }
+
+int read_file_data_from_fd(file_data_t *dst, const char *const path, int fd)
+{
+	const char *name = basename(path);
+
+	const int name_len = strlen(name);
+	if (name_len > NAME_MAX)
+		return -1;
+
+	memcpy(dst->name, name, name_len);
+	dst->name[name_len] = 0;
+
+    struct stat s;
+    if (fstat(fd, &s) < 0) {
+        perror("fstat");
+        return -1;
+    }
+
+    dst->size = s.st_size;
+    return 0;
+} 
+
+file_size_t bytes_to_size(size_t byte_size)
+{
+    double size = byte_size;
+    unsigned int idx = 0;
+
+	while (size >= 1024.0) {
+		idx++;
+		size /= 1024.0;
+	}
+
+    return (file_size_t) {
+        .size = size,
+        .unit_idx = idx,
+    };
+}
+
+ssize_t send_all(const void *const buf, size_t len, int soc)
+{
+	size_t sent = 0;
+
+	while (sent < len) {
+		const ssize_t s = send(soc, (void *)((uintptr_t)buf + sent),
+				       len - sent, 0);
+		if (s < 0)
+			return s;
+
+		sent += s;
+	}
+
+	return sent;
+}
+
+
--- core.h
+++ core.h
@@ -5,11 +5,22 @@
 
 typedef struct file_data {
 	off_t size;
-	char name[(long)NAME_MAX + 1];
+	char name[NAME_MAX + 1];
 } file_data_t;
 
 int read_file_data(file_data_t *dst, const char *const path);
+int read_file_data_from_fd(file_data_t *dst, const char *const path, int fd);
 
-#define ERR(source)                                                      \
-	(perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), \
+static const char file_size_units[4][4]= { "B\0", "kiB", "MiB", "TiB" };
+typedef struct file_size {
+    double size;
+    unsigned int unit_idx;
+} file_size_t;
+
+file_size_t bytes_to_size(size_t size);
+
+ssize_t send_all(const void *const buf, size_t len, int soc);
+
+#define ERR(source)                                                     \
+	(perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__),    \
 	 exit(EXIT_FAILURE))
